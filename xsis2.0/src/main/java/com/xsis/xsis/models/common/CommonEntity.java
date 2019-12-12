c89d5713f1112c869e08a3e216ad8d9780d2043a
ekosutrisno <ekosutrisno801@gmail.com>
2019-12-12 00:33:18 -0800

    Add Model Common Entity
    Menambahkan Common Entity untuk merecord aktifitas action yang terjadi pada setiap table. cara menggunakankannya tinggal extens disetiap model... thanks.

---
+++ xsis2.0/src/main/java/com/xsis/xsis/models/common/CommonEntity.java
@@ -0,0 +1,118 @@
+package com.xsis.xsis.models.common;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * CommonEntity
+ */
+
+/**
+ * Model CommonEntity digunakan untuk merecord History action yang terjadi pada
+ * tabel yang bersangkutan cara menggunakannya tinggal extend saja kesetiap
+ * model... thanks ALl. By Eko sutrisno
+ */
+
+@MappedSuperclass
+public class CommonEntity {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", nullable = false)
+    private Date createdOn;
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_on", nullable = true)
+    private Date modifiedOn;
+    @Column(name = "modified_by", nullable = true)
+    private String modifiedBy;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "deleted_on", nullable = true)
+    private Date deleteddOn;
+    @Column(name = "deleted_by", nullable = true)
+    private String deletedBy;
+
+    @Column(name = "isdelete", nullable = false)
+    private Boolean isDelete = false;
+
+    public CommonEntity() {
+    }
+
+    public CommonEntity(Date createdOn, String createdBy, Date modifiedOn, String modifiedBy, Date deleteddOn,
+            String deletedBy, Boolean isDelete) {
+        this.createdOn = createdOn;
+        this.createdBy = createdBy;
+        this.modifiedOn = modifiedOn;
+        this.modifiedBy = modifiedBy;
+        this.deleteddOn = deleteddOn;
+        this.deletedBy = deletedBy;
+        this.isDelete = isDelete;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(Date modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Date getDeleteddOn() {
+        return deleteddOn;
+    }
+
+    public void setDeleteddOn(Date deleteddOn) {
+        this.deleteddOn = deleteddOn;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public Boolean getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+}
